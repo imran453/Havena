7940d93dbfd5ddb92d35ed7d47b4a4d52e877164
imran453 <musaimrana226@gmail.com>
2026-06-29 09:22:27 +0100

    Updated components and fixed layout

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ export const Logo = ({ light = false, className = '' }) => {
   const text = light ? '#ffffff' : '#0f5e3a'
   return (
     <div className={`flex items-center gap-2 ${className}`}>
-      <img src="/images/logo.svg" alt="Havena logo" className="size-18 shrink-0" />
+      <img src="/images/logo.svg" alt="Havena logo" className="size-16 shrink-0" />
     
     </div>
   )
